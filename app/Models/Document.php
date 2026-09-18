7542028b8b10e57a08d988f904ed9673352eff57
Samsul Muarrif <samuarrif@gmail.com>
2026-09-18 20:31:39 +0700

    feat: add document relationships

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Document extends Model
 {
@@ -18,4 +19,9 @@ class Document extends Model
         'document_type',
         'visibility',
     ];
+
+    public function researchProject(): BelongsTo
+    {
+        return $this->belongsTo(ResearchProject::class);
+    }
 }
