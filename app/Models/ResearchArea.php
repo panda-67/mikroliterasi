cf1eae8b136b5bada02dd661854c4621d5637a45
Samsul Muarrif <samuarrif@gmail.com>
2026-09-18 20:24:46 +0700

    feat: add research area relationships

--- app/Models/ResearchArea.php
+++ app/Models/ResearchArea.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ResearchArea extends Model
 {
@@ -15,4 +16,12 @@ class ResearchArea extends Model
         'slug',
         'description',
     ];
+
+    public function researchProjects(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            ResearchProject::class,
+            'research_project_research_area'
+        );
+    }
 }
