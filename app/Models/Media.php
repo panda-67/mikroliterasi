5bbdcc60d78be63e1cbb1274913d773cb448abac
Samsul Muarrif <samuarrif@gmail.com>
2026-09-18 20:33:06 +0700

    feat: add media polymorphic relationship

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 
 class Media extends Model
 {
@@ -24,5 +25,10 @@ class Media extends Model
         return [
             'taken_at' => 'date',
         ];
+    }
+
+    public function mediable(): MorphTo
+    {
+        return $this->morphTo();
     }
 }
