f2bf1212d4e359b2962cb6eec7877e4ed6c6b2db
Samsul Muarrif <samuarrif@gmail.com>
2026-09-18 20:18:50 +0700

    feat: add research project relationships

--- app/Models/ResearchProject.php
+++ app/Models/ResearchProject.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class ResearchProject extends Model
 {
@@ -30,5 +34,49 @@ class ResearchProject extends Model
             'start_date' => 'date',
             'end_date' => 'date',
         ];
+    }
+
+    public function creator(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function people(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Person::class,
+            'research_project_people'
+        )->withPivot('role');
+    }
+
+    public function researchAreas(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            ResearchArea::class,
+            'research_project_research_area'
+        );
+    }
+
+    public function publications(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            Publication::class,
+            'research_project_publication'
+        );
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function documents(): HasMany
+    {
+        return $this->hasMany(Document::class);
+    }
+
+    public function media(): MorphMany
+    {
+        return $this->morphMany(Media::class, 'mediable');
     }
 }
